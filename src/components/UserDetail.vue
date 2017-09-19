31da8f04e9f458174cdcb034e9e6b622ea8d7246
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-09-19 11:02:10 -0300

    fix

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,12 +2,23 @@
   <div>
     <h3>You may view the User Details here</h3>
     <p>User Name: {{ name }}</p>
+    <p>{{ switchName() }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['name']
+  props: {
+    name: {
+      type: String,
+      default: 'João'
+    }
+  },
+  methods:{
+    switchName(){
+      return this.name.split("").reverse();
+    }
+  }
 }
 </script>
 
