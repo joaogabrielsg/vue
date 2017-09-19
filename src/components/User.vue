31636f348a74d1897d3e5b4902a7a5e1217ecc08
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-09-19 10:37:19 -0300

    communication between components

---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <app-user-detail :name = "name"></app-user-detail>
+</template>
+
+<script>
+import UserDetail from './UserDetail.vue'
+
+export default {
+  data () {
+    return {
+      name: 'Gabriel'
+    }
+  },
+  components:{
+    'app-user-detail': UserDetail
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>You may view the User Details here</h3>
+    <p>User Name: {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name']
+}
+</script>
+
+<style lang="scss">
+</style>
