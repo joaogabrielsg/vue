6ec545fea1aeb5530a002624aa4449730c3fabaf
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-09-28 12:20:18 -0300

    Using Forms

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
-    <app-user></app-user>
+  <div> 
+    <app-form></app-form>
   </div>
 </template>
 
 <script>
 import User from './components/User.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'app',
 
   components:{
-    'app-user': User
+    'app-user': User,
+    'app-form': Form
   }
 }
 </script>
---
+++ src/components/Form.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <form>
+            <label for="Email">Email</label>
+            <input 
+                    type="text"
+                    v-model="userData.email">
+
+            <label for="Password">Password</label>
+            <input 
+                    type="text"
+                    v-model="userData.password">
+
+            <label for="Age">Age</label>
+            <input 
+                    type="number"
+                    v-model="userData.age">
+        </form>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            userData:{
+                email: '',
+                password: '',
+                age: 27
+            }
+        }
+    }
+}
+</script>
