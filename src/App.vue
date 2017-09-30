39f27150ac1e11dcac7ef6b914b7d3304fd1f3fd
joaogabrielsg <joaogabrieldosantosgoncalves@gmail.com>
2017-09-30 14:43:43 -0300

    Custom directives

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div>
-    <app-user></app-user>
+    <!-- <app-user></app-user> -->
+
+    <!-- <label for="sendmail">
+      <input type="checkbox" id="sendail" value="send a mail" v-model="sendMail"> Send Mail
+    </label>
+
+    <label for="sendinfomail">
+      <input type="checkbox" id="sendinfomail" value="send information mail" v-model="sendMail"> Send InfoMail
+    </label>
+  
+    <hr>
+
+    <ul>
+      <li v-for="mail in sendMail">{{ mail }}</li>
+    </ul>
+
+    <hr>
+
+    <input type="radio" value="male" v-model="gener">Male
+
+    <input type="radio" value="female" v-model="gener">Female  
+
+    <hr>
+
+    <select name="City">
+      <option v-for="city in cities" :selected="city == 'Rio de Janeiro'">{{ city }}</option>
+    </select> -->
+
+    <app-mydirectives v-highlight></app-mydirectives>
+
   </div>
 </template>
 
 <script>
+
 import User from './components/User.vue'
+import MyDirectives from './components/MyDirectives.vue'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      sendMail: [],
+      gener: 'male',
+      cities: ['Fortaleza', 'Rio de Janeiro', 'São Paulo']
+    }
+  },
 
   components:{
-    'app-user': User
+    'app-user': User,
+    'app-mydirectives': MyDirectives
   }
 }
 </script>
---
+++ src/components/MyDirectives.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+      <p v-text="'Some Text'"></p>
+      <p v-html="'<strong>Some Strong Text</strong>'"></p>
+  </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
