d488c55309bc38be50f78f8a53286009558bbf79
joaogabrielsg <joaogabrieldosantosgoncalves@gmail.com>
2017-09-30 16:01:55 -0300

    Implement Delay

--- src/main.js
+++ src/main.js
@@ -7,15 +7,18 @@ Vue.directive('highlight', {
 
     // el.style.backgroundColor = binding.value;
 
-    // if(binding.arg == 'background'){
-    //   el.style.backgroundColor = binding.value;
-    // }else{
-    //   el.style.color = binding.value;
-    // }
 
+    var delay = 0;
     if(binding.modifiers['delayed']){
-      
+      delay = 3000;
     }
+    setTimeout(() => {
+      if(binding.arg == 'background'){
+        el.style.backgroundColor = binding.value;
+      }else{
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
