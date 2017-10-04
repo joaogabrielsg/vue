9a35fef677487f69fb73fd17130e2e76f0268e33
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2017-10-04 12:35:26 -0300

    Starting Animation

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -3,7 +3,15 @@
       <h1>Animations</h1>
       <hr>
       <button @click="show = !show">Show Alert</button>
-      <transition>
+      <transition name="fade">
+          <div v-if="show">This is some info</div>
+      </transition>
+
+      <transition name="slide" type="animation">
+          <div v-show="show">This is some info</div>
+      </transition>
+
+      <transition name="slide" appear>
           <div v-if="show">This is some info</div>
       </transition>
   </div>
@@ -13,9 +21,57 @@
 export default {
     data(){
         return{
-            show: false
+            show: true
         }
     }
 }
     
 </script>
+
+<style>
+    .fade-enter{
+        opacity: 0;
+    }  
+    .fade-enter-active{
+        transition: opacity 1s;
+    }
+    .fade-leave{
+        /*opacity: 1; Default Value*/
+    }
+    .fade-leave-active{
+        transition: opacity 1s;
+        opacity: 0;
+    }
+    .slide-enter{
+        opacity: 0;
+    }  
+    .slide-enter-active{
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity 0.5s;
+    }
+    .slide-leave{
+    }
+    .slide-leave-active{
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from{
+            transform: translateY(20px);
+        }
+        to{
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        to{
+            transform: translateY(20px);
+        }
+        from{
+            transform: translateY(0);
+        }
+    }
+</style>
